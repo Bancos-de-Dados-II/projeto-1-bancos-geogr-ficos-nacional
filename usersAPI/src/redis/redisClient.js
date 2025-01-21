a05b186573f6afc6ae1cff79ba874c833e8f9a7d
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2025-01-21 08:21:37 -0300

    fix: ajustes

--- usersAPI/src/redis/redisClient.js
+++ usersAPI/src/redis/redisClient.js
@@ -2,4 +2,9 @@ import Redis from "ioredis";
 
 const redisClient = new Redis();
 
+//monitorando erros
+redisClient.on('error', (error) => {
+    console.log(`Erro ao conectar com o redis: ${error.message}`);
+})
+
 export default redisClient;
